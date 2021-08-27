937bb888f683c2cbb261908205e8e87179786ffa
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-08-26 19:31:55 -0600

    Fix 90%toggle()

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 //NASA KEY = 7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL
-const $blueHour = $('#blue-hour')
+
+//APOD Data
+const $apodData = $('#apod-data')
+const $hideAPODData = $('hide-apod-data')
 const $photo = $('#photo')
 const $photoTitle = $('#photo-title')
 const $copyright = $('#copyright')
@@ -7,25 +10,29 @@ const $explanation = $('#explanation')
 const $date = $('#date')
 const $section = $('section')
 
-function handleEvent(event) {
+function handleAPODEvent(event) {
     event.preventDefault();
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL`
     }).then((data) => {
-        console.log(data)
+        //check data populates
+        //console.log(data)
         $photoTitle.text(data.title)
         $photo.attr('src', data.hdurl)
         $copyright.text(`Image produced by: ${data.copyright}`)
         $date.text(`Photo taken: ${data.date}`)
         $explanation.text(data.explanation)
-        $blueHour.on('click', function () {
-            $section.toggle()
-        })
+        //toggle still wants two clicks when first loaded, otherwise it works fine
+        $section.toggle()
+          
     },
         (error) => {
             console.log('There is a problem', error)
         });
 }
-$blueHour.on('click', handleEvent)
+$apodData.on('click', handleAPODEvent)
+
+
+
 
 
