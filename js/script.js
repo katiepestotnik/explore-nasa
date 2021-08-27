75518ebdc5fb6c475de89d0ec89a68392945c162
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-08-27 12:32:54 -0600

    Cleaned up look and button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 //NASA KEY = 7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL
 
 //APOD Data
-const $apodData = $('#apod-data')
-const $hideAPODData = $('hide-apod-data')
-const $photo = $('#photo')
-const $photoTitle = $('#photo-title')
-const $copyright = $('#copyright')
-const $explanation = $('#explanation')
-const $date = $('#date')
-const $section = $('section')
+let apodData;
+const $apodData = $('#apod-button');
+const $photo = $('#photo');
+const $photoTitle = $('#photo-title');
+const $copyright = $('#copyright');
+const $explanation = $('#explanation');
+const $date = $('#date');
+const $section = $('section');
 
 function handleAPODEvent(event) {
     event.preventDefault();
@@ -17,20 +17,23 @@ function handleAPODEvent(event) {
     }).then((data) => {
         //check data populates
         //console.log(data)
-        $photoTitle.text(data.title)
-        $photo.attr('src', data.hdurl)
-        $copyright.text(`Image produced by: ${data.copyright}`)
-        $date.text(`Photo taken: ${data.date}`)
-        $explanation.text(data.explanation)
-        //toggle still wants two clicks when first loaded, otherwise it works fine
-        $section.toggle()
-          
+        apodData = data;
+        renderAPOD();    
     },
         (error) => {
-            console.log('There is a problem', error)
+            console.log('There is a problem', error);
         });
-}
-$apodData.on('click', handleAPODEvent)
+};
+function renderAPOD() {
+    $photoTitle.text(apodData.title);
+    $photo.attr('src', apodData.hdurl);
+    $copyright.text(`Image produced by: ${apodData.copyright}`);
+    $date.text(`Photo taken: ${apodData.date}`);
+    $explanation.text(apodData.explanation);
+        //toggle still wants two clicks when first loaded, otherwise it works fine
+    $section.toggle();
+};
+$apodData.on('click', handleAPODEvent);
 
 
 
