abd6dd9cf7fe3567d965fc41607f6989ee8f7c04
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-08-28 08:55:22 -0600

    Refactor and repaired some format with html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //NASA KEY = 7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL
-
 //APOD Data
 let apodData;
 const $apodData = $('#apod-button');
@@ -9,56 +8,65 @@ const $copyright = $('#copyright');
 const $explanation = $('#explanation');
 const $date = $('#date');
 const $section = $('section');
-
+//APOD Click function with API call
 function handleAPODEvent(event) {
     event.preventDefault();
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL`
     }).then((data) => {
-        console.log(data)
+        //console.log(data)
         apodData = data;
         renderAPOD();
-//toggle still wants two clicks when first loaded, otherwise it works fine //
-        $section.toggle();
+        //fix if copyright is undefined
+//toggle working better with each api data set put into own divs //
     },
         (error) => {
             console.log('There is a problem', error);
         });
 };
+//function for APOD click function to insert data
 function renderAPOD() {
     $photoTitle.text(apodData.title);
     $photo.attr('src', apodData.hdurl);
     $copyright.text(`Image produced by: ${apodData.copyright}`);
     $date.text(`Photo taken: ${apodData.date}`);
     $explanation.text(apodData.explanation);
+    $section.toggle();
 };
-$apodData.on('click', handleAPODEvent);
-
-///New Button for EPIC data
+///EPIC data
+let epicData;
 const $epicData = $('#epic-button');
 const $epicSection =$('#section-one')
 const $epicPhoto = $('#epic-photo');
 const $epicCaption = $('#epic-caption');
-const $epicIdentifier =$('#epic-identifier')
+const $epicIdentifier = $('#epic-identifier');
 const $epicTime = $('#epic-date-time');
+//EPIC click function with API call
 function handleEPICEvent(event) {
-    event.preventDefault()
+    event.preventDefault();
     $.ajax({
         url: `https://api.nasa.gov/EPIC/api/natural/date/2021-02-11?api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL`
     }).then((data) => {
-        $epicPhoto.attr('src', "https://api.nasa.gov/EPIC/archive/natural/2021/02/11/png/epic_1b_20210211015604.png?api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL");
-        $epicCaption.text(data[5].caption)
-        $epicIdentifier.text(`Identifier #: ${data[5].identifier}. Verifies photo time and date.`)
-        $epicTime.text(`Date and time of image capture: ${data[5].date}.`)
-        //toggle function also creates issue with two click after initial load
-        $epicSection.toggle()
+        epicData = data;
+        renderEpic();
     },
         (error) => {
             console.log('There is a problem', error);
         });
 };
-$epicData.on('click', handleEPICEvent)
+//function for EPIC click function to insert data
+function renderEpic() {
+    $epicPhoto.attr('src', `https://api.nasa.gov/EPIC/archive/natural/2021/02/11/png/${epicData[5].image}.png?api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL`);
+    //console.log(epicData[5].image);
+    $epicCaption.text(epicData[5].caption);
+    $epicIdentifier.text(`Identifier #: ${epicData[5].identifier}. Verifies photo time and date.`);
+    $epicTime.text(`Date and time of image capture: ${epicData[5].date}.`);
+    //toggle function also creates issue with two click after initial load
+    $epicSection.toggle();
+};
+
+//All Click Functions
+$apodData.on('click', handleAPODEvent);
+$epicData.on('click', handleEPICEvent);
 
 //<img style="width:75%;" src="https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20210211004104.png?api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL">
-
-
