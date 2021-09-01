c706abbcdc5bcd8f5f59b8a0b75543b26ac32eb1
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-08-31 18:41:38 -0600

    Add and Style Input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-//Next step: Find an API where the user can search for an image/data if possible add input 
-
 //NASA KEY = 7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL
 //APOD Data
 let apodData;
@@ -39,13 +37,11 @@ function renderAPOD() {
 };
 ///EPIC data
 let epicData;
-//Each toggle will randomly pull a different array element to show multiple images and times
-//epicData length was 11 
 const randomEpicDataGenerator = () => {
-    return Math.floor(Math.random() * 11);
+    return Math.floor(Math.random() * epicData.length);
 };
 const $epicData = $('#epic-button');
-const $epicSection = $('#section-one');
+const $epicSection = $('#epic-section');
 const $epicTitle = $('#epic-title');
 const $epicPhoto = $('#epic-photo');
 const $epicCaption = $('#epic-caption');
@@ -65,67 +61,64 @@ function handleEPICEvent(event) {
             console.log('There is a problem', error);
         });
 };
-//function for EPIC click function to insert data
+//Function to insert Epicdata into click function
 function renderEpic() {
     $epicTitle.text(`Feburary 11, 2021 Lunar Transit Images`);
     $epicPhoto.attr('src', `https://api.nasa.gov/EPIC/archive/natural/2021/02/11/png/${epicData[randomEpicDataGenerator()].image}.png?api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL`);
     $epicCaption.text(`${epicData[randomEpicDataGenerator()].caption}.`);
     $epicIdentifier.text(`Identifier #: ${epicData[randomEpicDataGenerator()].identifier}`);
     $epicTime.text(`Date and time of image capture: ${epicData[randomEpicDataGenerator()].date}`);
-    //toggle function also creates issue with two click after initial load
     $epicSection.toggle();
 };
-//had to determine how to grab image without a url property
-//<img style="width:75%;" src="https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20210211004104.png?api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL">
-
-
-
-
-
-
-
-
 //Rover Data
 let roverData;
 const $roverImage = $('#rover-image');
-const $roverId = $('#rover-id');
-//const $roverSol = $('#rover-sol');
-//Is this the best way to grab userInput for my ajax api call?
+const $roverId = $('#rover-earth-data');
+const $roverName = $('#rover-name');
 const $userInput = $('#user-input');
-
+const $roverCamera = $('#rover-camera');
+const $roverTitle = $('#rover-title')
+const $roverStyle = $('.rover-style')
+//Produce random photo from rover array 
+const randomRoverDataGenerator = () => {
+    return Math.floor(Math.random() * roverData.photos.length);
+};
 //Rover click function with API call
 function handleRoverEvent(event) {
     event.preventDefault();
     if ($userInput.val() === undefined || $userInput.val() === '') {
-        alert('fix input')
+        alert('Invalid Input: enter either Curiosity or Opportunity');
     } else {
         // can't put userInput for the date, works fine with date=2015-6-3&api_key= 
         $.ajax({
-            url: `https://api.nasa.gov/mars-photos/api/v1/rovers/${$userInput.val()}/photos?earth_date=2015-6-3&api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL`
+            url: `https://api.nasa.gov/mars-photos/api/v1/rovers/${$userInput.val()}/photos?earth_date=2017-04-26&api_key=7t9znEI8iqcIiSc81GpcDqZ0KlrVfhCSz8PkEOOL`
         }).then((data) => {
             roverData = data;
             console.log(roverData);
             renderRover();
             //Clear after submit
-            //$userInput.val('');
-            //toggle working better with each api data set put into own divs //
+            $userInput.val('');
         },
             (error) => {
-                if ($userInput === undefined) {
-                    alert('reenter valid input')
+                console.log('There is a problem', error)
                 }
-            });
-    }
+            );
+    };
 };
-//function for rover submit function to insert data
-//data not being pulled correctly
+//function insert roverData during submit 
 function renderRover() {
-    //can't read properties not sure why
-    $roverImage.attr('src', roverData.photos[0].img_src);
-    $roverId.text(roverData.photos[0].id);
-    //$roverSol.text(roverData[0].sol);
+    $roverTitle.show();
+    $roverName.show();
+    $roverCamera.show();
+    $roverStyle.show();
+    $roverImage.attr('src', roverData.photos[randomRoverDataGenerator()].img_src);
+    $roverName.text(roverData.photos[0].rover.name);
+    $roverCamera.text(roverData.photos[randomRoverDataGenerator()].camera.full_name);
 };
 //All Click Functions
 $apodData.on('click', handleAPODEvent);
 $epicData.on('click', handleEPICEvent);
 $('form').on('submit', handleRoverEvent);
+
+// curiosity length 40
+// oppo length 34
